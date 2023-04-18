16195556d2f07ee9153c9c3bd7b60410e74036bc
jack-king1 <jackk@neonplay.com>
2023-04-19 00:41:14 +0100

    working on adding api and champion model

---
+++ APIProject/APIProject/Controllers/ChampionController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using APIProject.Models;
+
+namespace APIProject.Controllers
+{
+    public class ChampionController : Controller
+    {
+        public IActionResult Index()
+        {
+            Champion model = new Champion(0, "aatrox");
+            return View(model);                
+        }                                
+
+        public IActionResult ChampSelect(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+    }
+}
--- APIProject/APIProject/Models/Champion.cs
+++ APIProject/APIProject/Models/Champion.cs
@@ -1,9 +1,28 @@
-﻿namespace APIProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace APIProject.Models
 
 {
     public class Champion
     {
+        public int ChampionId { get; set; }
+        public string championName { get; set; }
+        public string championImage { get; set; }
+
+
         //Constructor
-        private Champion() { }
+        public Champion() {
+            ChampionId = -1;
+            championName = string.Empty;
+            championImage = string.Empty;   
+
+        }
+
+        public Champion(int id, string name, string image = "")
+        {
+            ChampionId = id;
+            championName = name;
+            championImage = image;
+
+        }
     }
 }
